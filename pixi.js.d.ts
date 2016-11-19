f396b510878510b85b1c9891ea6a807ff01df608
Alex Davies <alex@codecutout.com>
2016-11-19 14:38:38 +0000

    Defined PIXI.Container addChild return types to match the input type.

--- pixi.js.d.ts
+++ pixi.js.d.ts
@@ -228,9 +228,9 @@ declare module PIXI {
         height: number;
 
         protected onChildrenChange: (...args: any[]) => void;
-        addChild(child: DisplayObject): DisplayObject;
+        addChild<T extends DisplayObject>(child: T, ): T;
         addChild(...child: DisplayObject[]): DisplayObject;
-        addChildAt(child: DisplayObject, index: number): DisplayObject;
+        addChildAt<T extends DisplayObject>(child: T, index: number): T;
         swapChildren(child: DisplayObject, child2: DisplayObject): void;
         getChildIndex(child: DisplayObject): number;
         setChildIndex(child: DisplayObject, index: number): void;
